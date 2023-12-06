78b53c97102d035f53749cdab690f12ba98d1fc3
siriwattana29 <148177901+siriwattana29@users.noreply.github.com>
2023-12-06 15:32:56 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from firebase_admin import firestore
 
@@ -12,35 +13,36 @@ def app():
     if st.session_state.username == '':
         st.header(' :violet[Login to be able to CREATE!!] ')
     else:
-        ph = 'TODO something'    
+        ph = 'KBM'    
 
-        todo = st.text_area(label = ' :orange[+ New TODO]', placeholder = ph, height = None, max_chars = 500)
+        todo = st.text_area(label = ' :orange[+ New KBM ]', placeholder = ph, height = None, max_chars = 500)
     
         if st.button('Create', use_container_width = 20):
             if todo != '':
-                info = db.collection('TODOs').document(st.session_state.username).get()
+                info = db.collection('KBMs').document(st.session_state.username).get()
                 if info.exists:
                     info = info.to_dict()
                     if 'Content' in info.keys():
-                        t = db.collection('TODOs').document(st.session_state.username)
+                        t = db.collection('KBMs').document(st.session_state.username)
                         t.update({u'Content': firestore.ArrayUnion([u'{}'.format(todo)])})
                     else:
                         data = {"Content":[todo],'Username':st.session_state.username}
-                        db.collection('TODOs').document(st.session_state.username).set(data)    
+                        db.collection('KBMs').document(st.session_state.username).set(data)    
                 else:
                     data = {"Content":[todo],'Username':st.session_state.username}
-                    db.collection('TODOs').document(st.session_state.username).set(data)
+                    db.collection('KBMs').document(st.session_state.username).set(data)
                     
-                st.success('TODO uploaded!!')
+                st.success('KBM uploaded!!')
         
-        st.header(' :violet[Latest TODOs] ')
+        st.header(' :violet[Latest KBMs] ')
         
    
-    docs = db.collection('TODOs').document(st.session_state['username']).get()
+    docs = db.collection('KBMs').document(st.session_state['username']).get()
     doc = docs.to_dict()
     content = doc['Content']
     try:
         for c in range(len(content)-1, -1, -1):
-            st.text_area(label = ':green[TODOs by:] ' + ':orange[{}]'.format(st.session_state.username), value = content[c], height = 20,)
+            st.text_area(label = ':green[KBMs by:] ' + ':orange[{}]'.format(st.session_state.username), value = content[c], height = 20,)
     except: pass
-    
+
+
